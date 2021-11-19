87d18ef04d8ef1a14a60b7063d8e22050ceccff2
Diego <diegocorf123@gmail.com>
2021-11-19 10:33:32 -0600

    Add layout components

---
+++ src/components/Layout.tsx
@@ -0,0 +1,29 @@
+import Router from "next/router";
+import Head from "./Head";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import { FunctionComponent } from "react";
+
+interface LayoutProps {
+  pageTitle: string;
+  currentRoute: string;
+  metaDescription: string;
+}
+
+const Layout: FunctionComponent<LayoutProps> = ({
+  children,
+  pageTitle,
+  currentRoute,
+  metaDescription,
+}) => {
+  return (
+    <div>
+      <Head pageTitle={pageTitle} description={metaDescription} />
+      <NavBar currentRoute={currentRoute} />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar: React.FC<{ currentRoute: string }> = ({ currentRoute }) => {
+  return <div></div>;
+};
+
+export default NavBar;
